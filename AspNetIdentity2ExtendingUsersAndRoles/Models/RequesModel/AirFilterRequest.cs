942e9c1248cc1f2227c8e5f7321338334de8e1ee
asaddurrani <asaddurrani@hotmail.com>
2016-10-13 09:55:14 -0700

    asas

---
+++ AspNetIdentity2ExtendingUsersAndRoles/Models/RequesModel/AirFilterRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetIdentity2ExtendingUsersAndRoles.Models.RequesModel
+{
+    public class AirFilterRequest : GetPagedListRequest
+    {
+        public int? AirFilterId { get; set; }
+
+    }
+}
